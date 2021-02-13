bd6091114c1fc2923eb25edebd51b59665716d8f
Sergey <52304443+c29r3@users.noreply.github.com>
2021-02-13 14:14:41 +0300

    Update validator-fee-to-validator-address.sh

--- validator-fee-to-validator-address.sh
+++ validator-fee-to-validator-address.sh
@@ -5,6 +5,6 @@ KEYS_PATH="/home/solana-mb/solana"
 RPC="http://localhost:8999"
 
 BALANCE=$(${BIN_FILE} balance ${KEYS_PATH}/vote-account-keypair.json -u ${RPC} | awk -F' ' '{print $1}')
-BALANCE=$(echo "${BALANCE} - 0.5" | bc)
+BALANCE=$(echo "${BALANCE} - $(seq 0 .02 1 | shuf | head -n1)" | bc)
 
 ${BIN_FILE} withdraw-from-vote-account ${KEYS_PATH}/vote-account-keypair.json ${KEYS_PATH}/validator-keypair.json ${BALANCE} --authorized-withdrawer ${KEYS_PATH}/vote-account-keypair.json -u ${RPC}
