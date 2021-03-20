0984e370adfb75a2ea31d4f654586fc3d8a46743
Sergey <52304443+c29r3@users.noreply.github.com>
2021-03-20 19:36:54 +0300

    Update validator-fee-to-validator-address.sh

--- validator-fee-to-validator-address.sh
+++ validator-fee-to-validator-address.sh
@@ -4,7 +4,7 @@ BIN_FILE="/home/solana-mb/.local/share/solana/install/active_release/bin/solana"
 KEYS_PATH="/home/solana-mb/solana"
 RPC="http://localhost:8999"
 
-sleep $(shuf -i 1-1021 -n 1)
+sleep $(shuf -i 1-321 -n 1)
 
 BALANCE=$(${BIN_FILE} balance ${KEYS_PATH}/vote-account-keypair.json -u ${RPC} | awk -F' ' '{print $1}')
 BALANCE=$(echo "${BALANCE} - $(seq 0 .02 1 | shuf | head -n1)" | bc)
