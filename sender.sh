f55551d2b5b573f509a78db75fab4313e4504642
Sergey <52304443+c29r3@users.noreply.github.com>
2021-06-22 01:07:46 +0300

    Update sender.sh

--- sender.sh
+++ sender.sh
@@ -10,15 +10,15 @@ ADDR=$1
 
 cd ${KEY_PATH}
 
-BALANCE=$(${BIN_FILE} balance validator-keypair.json -u ${RPC} | cut -d " " -f1)
+BALANCE=$(${BIN_FILE} balance validator-keypair.json | cut -d " " -f1)
 BALANCE=$(echo "${BALANCE} - $(shuf -i 19-22 -n 1)" | bc)
 
 echo -e "\n\n" | ${BIN_FILE}-keygen new -o ${PROXY_KEYPAIR}
-solana transfer --keypair validator-keypair.json ${PROXY_KEYPAIR} ${BALANCE} --url ${RPC} --allow-unfunded-recipient
+solana transfer --keypair validator-keypair.json ${PROXY_KEYPAIR} ${BALANCE} --allow-unfunded-recipient
 sleep 10
 
-BALANCE=$(${BIN_FILE} balance ${PROXY_KEYPAIR} -u ${RPC} | cut -d " " -f1)
+BALANCE=$(${BIN_FILE} balance ${PROXY_KEYPAIR} | cut -d " " -f1)
 SEND_AMOUNT=$(echo "${BALANCE} - $(seq 0 .0001 0.0005 | shuf | head -n1)" | bc)
-solana transfer --keypair ${PROXY_KEYPAIR} ${ADDR} ${SEND_AMOUNT} --url ${RPC} --allow-unfunded-recipient
+solana transfer --keypair ${PROXY_KEYPAIR} ${ADDR} ${SEND_AMOUNT} --allow-unfunded-recipient
 
-echo $(${BIN_FILE} balance ${PROXY_KEYPAIR} -u ${RPC} | cut -d " " -f1)
+echo $(${BIN_FILE} balance ${PROXY_KEYPAIR} | cut -d " " -f1)
