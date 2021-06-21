f4974132230362db69e02c3625bea1ac1f0962ad
Sergey <52304443+c29r3@users.noreply.github.com>
2021-06-22 01:08:22 +0300

    Update validator-fee-to-validator-address.sh

--- validator-fee-to-validator-address.sh
+++ validator-fee-to-validator-address.sh
@@ -6,7 +6,7 @@ RPC="http://localhost:8999"
 
 #sleep $(shuf -i 1-321 -n 1)
 
-BALANCE=$(${BIN_FILE} balance ${KEYS_PATH}/vote-account-keypair.json -u ${RPC} | awk -F' ' '{print $1}')
+BALANCE=$(${BIN_FILE} balance ${KEYS_PATH}/vote-account-keypair.json | awk -F' ' '{print $1}')
 BALANCE=$(echo "${BALANCE} - $(seq 0 .02 1 | shuf | head -n1)" | bc)
 
-${BIN_FILE} withdraw-from-vote-account ${KEYS_PATH}/vote-account-keypair.json ${KEYS_PATH}/validator-keypair.json ${BALANCE} --authorized-withdrawer ${KEYS_PATH}/vote-account-keypair.json -u ${RPC}
+${BIN_FILE} withdraw-from-vote-account ${KEYS_PATH}/vote-account-keypair.json ${KEYS_PATH}/validator-keypair.json ${BALANCE} --authorized-withdrawer ${KEYS_PATH}/vote-account-keypair.json
